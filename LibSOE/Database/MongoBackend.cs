d12c4d8230aab49f29c7e8a33ad478eb348422c1
Joshua Scott <admin@joshsora.com>
2015-09-16 16:24:48 +0100

    Database: Start the Mongo backend

---
+++ LibSOE/Database/MongoBackend.cs
@@ -0,0 +1,16 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace SOE.Database
+{
+    class MongoBackend : IDatabaseBackend
+    {
+        private readonly IMongoClient client;
+        private readonly IMongoDatabase database;
+
+        public void Connect(string host, int port)
+        {
+            
+        }
+    }
+}
