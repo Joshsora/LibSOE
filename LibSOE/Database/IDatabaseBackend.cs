b9b47fe6cfbc7321cf812cfae6b8a761140395bd
Joshua Scott <admin@joshsora.com>
2015-09-16 18:21:43 +0100

    Database: Add collection existance/creation stuff

--- LibSOE/Database/IDatabaseBackend.cs
+++ LibSOE/Database/IDatabaseBackend.cs
@@ -3,5 +3,6 @@
     public interface IDatabaseBackend
     {
         void Connect(string username, string password, string host, int port);
+        void Setup(string[] tables);
     }
 }
--- LibSOE/Database/MongoBackend.cs
+++ LibSOE/Database/MongoBackend.cs
@@ -1,4 +1,6 @@
-﻿using MongoDB.Bson;
+﻿using System.Linq;
+using System.Threading.Tasks;
+using MongoDB.Bson;
 using MongoDB.Driver;
 
 namespace SOE.Database
@@ -6,13 +8,34 @@ namespace SOE.Database
     class MongoBackend : IDatabaseBackend
     {
         private IMongoClient client;
-        private readonly IMongoDatabase database;
+        private IMongoDatabase database;
 
         public void Connect(string username, string password, string host, int port)
         {
             // Connect!
             string connectionString = string.Format("mongo://{0}:{1}@{2}:{3}/soe_db", username, password, host, port);
             client = new MongoClient(connectionString);
+
+            database = client.GetDatabase("soe_db");
+        }
+
+        public async void Setup(string[] collections)
+        {
+            foreach (string collectionName in collections)
+            {
+                bool collectionExists = await CollectionExistsAsync(collectionName);
+                if (!collectionExists)
+                {
+                    await database.CreateCollectionAsync(collectionName);
+                }
+            }
+        }
+
+        private async Task<bool> CollectionExistsAsync(string collectionName)
+        {
+            var filter = new BsonDocument("name", collectionName);
+            var collections = await database.ListCollectionsAsync(new ListCollectionsOptions { Filter = filter });
+            return (await collections.ToListAsync()).Any();
         }
     }
 }
