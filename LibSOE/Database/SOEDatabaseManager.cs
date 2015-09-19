3939b49092c0daa268410a3a5ca93cdbdb0178cd
Joshua Scott <admin@joshsora.com>
2015-09-19 15:22:26 +0100

    Database: Add 'SOEDatabaseManager'

---
+++ LibSOE/Database/SOEDatabaseManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+
+namespace SOE.Database
+{
+    public class SOEDatabaseManager
+    {
+        private Dictionary<string, IDatabaseBackend> Databases;
+
+        public SOEDatabaseManager()
+        {
+            Databases = new Dictionary<string, IDatabaseBackend>();
+        }
+
+        public IDatabaseBackend GetDatabase(string name)
+        {
+            if (Databases.ContainsKey(name))
+            {
+                return Databases[name];
+            }
+
+            return null;
+        }
+
+        public void AddDatabase(string name, IDatabaseBackend database)
+        {
+            if (Databases.ContainsKey(name))
+            {
+                return;
+            }
+
+            Databases.Add(name, database);
+        }
+    }
+}
