5bf6b2f0cd0789a64892a93085a084924668b835
Joshua Scott <admin@joshsora.com>
2015-09-19 12:19:12 +0100

    Database: Added 'Query' to the Database Backend interface and implement a working version into the Mongo backend.

--- LibSOE/Database/IDatabaseBackend.cs
+++ LibSOE/Database/IDatabaseBackend.cs
@@ -1,8 +1,20 @@
-﻿namespace SOE.Database
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace SOE.Database
 {
     public interface IDatabaseBackend
     {
+        // Setup
         void Connect(string host, int port);
         void Setup(string[] tables);
+        
+        // Very non-specified command run
+        Task<TResult> RunCommand<TResult>(string table, string command);
+
+        // Very specific command runs
+        Task<TResult> Query<TResult>(string table, Dictionary<string, dynamic> filter) where TResult : new();
+        // Task<uint> Insert(string table, dynamic obj);
+        // Task<bool> Delete(string table, uint id);
     }
 }
--- LibSOE/Database/MongoBackend.cs
+++ LibSOE/Database/MongoBackend.cs
@@ -1,6 +1,10 @@
-﻿using System.Linq;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using MongoDB.Bson;
+using MongoDB.Bson.Serialization;
+using MongoDB.Bson.Serialization.Serializers;
 using MongoDB.Driver;
 
 namespace SOE.Database
@@ -12,7 +16,6 @@ namespace SOE.Database
 
         public void Connect(string host, int port)
         {
-            // Connect!
             string connectionString = string.Format("mongodb://{0}:{1}", host, port);
             client = new MongoClient(connectionString);
 
@@ -37,5 +40,71 @@ namespace SOE.Database
             var collections = await database.ListCollectionsAsync(new ListCollectionsOptions { Filter = filter });
             return (await collections.ToListAsync()).Any();
         }
+
+        public async Task<TResult> RunCommand<TResult>(string collection, string command)
+        {
+            return await database.RunCommandAsync<TResult>(command);
+        }
+
+        public async Task<TResult> Query<TResult>(string collectionName, Dictionary<string, dynamic> filter) where TResult : new()
+        {
+            // Get the collection
+            var collection = database.GetCollection<BsonDocument>(collectionName);
+
+            // Get the type name for the filter
+            string className = typeof (TResult).Name;
+
+            // Prepare the filter
+            BsonDocument completeFilter = new BsonDocument();
+            completeFilter.Add("_t", className);
+            if (filter.Any())
+            {
+                Dictionary<string, dynamic> newFilter = filter.ToDictionary(
+                    x => "fields." + x.Key,
+                    x => x.Value
+                );
+
+                completeFilter.Add(newFilter);
+            }
+
+            // Get our cursor, and go through the documents..
+            using (var cursor = await collection.FindAsync(completeFilter))
+            {
+                await cursor.MoveNextAsync();
+                var batch = cursor.Current;
+                if (!batch.Any())
+                {
+                    return new TResult();
+                }
+
+                // Get the document
+                foreach (var document in batch)
+                {
+                    if (!document.Contains("fields"))
+                    {
+                        // This is not an object..
+                        continue;
+                    }
+
+                    // Deserialize the object!
+                    return BsonSerializer.Deserialize<TResult>(document["fields"].ToBsonDocument());
+                }
+
+                // Didn't exist..
+                return new TResult();
+            }
+        }
+
+        /*
+        public async Task<uint> Insert(string collection, MongoStorable storable)
+        {
+            
+        }
+
+        public async Task<bool> Delete(string collection, uint id)
+        {
+            
+        }
+         */
     }
 }
