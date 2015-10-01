a68be7a0d3dee3a3a7009e9ecff58acdc63f7905
Joshua Scott <admin@joshsora.com>
2015-10-01 21:48:26 +0100

    Daemon: start the basis of the tool

---
+++ SOEDaemon/Config/DaemonOptions.cs
@@ -0,0 +1,38 @@
+﻿using System.Text;
+using CommandLine;
+using CommandLine.Text;
+
+namespace SOEDaemon
+{
+    internal class DaemonOptions
+    {
+        [Option('c', "config", DefaultValue = "config.cfg", HelpText = "The configuration for this daemon.")]
+        public string ConfigFile { get; set; }
+
+        [Option('v', "verbose", DefaultValue = false, HelpText = "Ouput daemon-specific messages to stdout.")]
+        public bool Verbose { get; set; }
+
+        [ParserState]
+        public IParserState LastParserState { get; set; }
+
+        [HelpOption]
+        public string GetUsage()
+        {
+            // Create the string builder
+            StringBuilder usage = new StringBuilder();
+
+            // Write the message
+            usage.AppendLine("LibSOE's daemon utility");
+            usage.AppendLine("https://github.com/Joshsora/LibSOE");
+            usage.AppendLine("Read the wiki pages for application-specific help!");
+            usage.AppendLine();
+
+            usage.AppendLine(HelpText.AutoBuild(this,
+                (current) => HelpText.DefaultParsingErrorsHandler(this, current))
+            );
+
+            // Return the help message
+            return usage.ToString();
+        }
+    }
+}
--- SOEDaemon/Program.cs
+++ SOEDaemon/Program.cs
@@ -1,10 +1,41 @@
-﻿namespace SOEDaemon
+﻿using System;
+using System.Collections.Generic;
+using CommandLine;
+using SOE.Core;
+
+namespace SOEDaemon
 {
     class Program
     {
+        private static List<SOEServer> Servers; 
+
         static void Main(string[] args)
         {
-            // TODO
+            DaemonOptions options = new DaemonOptions();
+
+            // Successful parse?
+            if (Parser.Default.ParseArguments(args, options))
+            {
+                // Are we verbose?
+                if (options.Verbose)
+                {
+                    // Log verbosely
+                    Log("Using configuration: {0}", options.ConfigFile);
+                }
+
+                // Configure!
+                Configure(options.ConfigFile);
+            }
+        }
+
+        static void Configure(string configFile)
+        {
+
+        }
+
+        static void Log(string message, params object[] args)
+        {
+            Console.WriteLine(":SOEDaemon: " + message, args);
         }
     }
 }
